0bea27550085c43e99b7dfafdf9cdb0e0b26c743
DEVIN TASSIE <devintassie@DEVINs-MacBook-Pro.local>
2017-10-28 12:31:40 -0500

    c9 wip

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,21 +1,21 @@
 (function() {
     function seekBar($document) {
 
-    var calculatePercent = function(seekBar, event) {
-    var offsetX = event.pageX - seekBar.offset().left;
-    var seekBarWidth = seekBar.width();
-    var offsetXPercent = offsetX / seekBarWidth;
-    offsetXPercent = Math.max(0, offsetXPercent);
-    offsetXPercent = Math.min(1, offsetXPercent);
-    return offsetXPercent;
-};
+      var calculatePercent = function(seekBar, event) {
+       var offsetX = event.pageX - seekBar.offset().left;
+       var seekBarWidth = seekBar.width();
+       var offsetXPercent = offsetX / seekBarWidth;
+       offsetXPercent = Math.max(0, offsetXPercent);
+       offsetXPercent = Math.min(1, offsetXPercent);
+       return offsetXPercent;
+   };
 
-      return {
-         templateUrl: '/templates/directives/seek_bar.html',
-         replace: true,
-         restrict: 'E',
-         scope: { },
-         link: function(scope, element, attributes) {
+   return {
+       templateUrl: '/templates/directives/seek_bar.html',
+       replace: true,
+       restrict: 'E',
+       scope: { },
+       link: function(scope, element, attributes) {
            scope.value = 0;
            scope.max = 100;
 
@@ -33,26 +33,29 @@
            };
 
            scope.onClickSeekBar = function(event) {
-            var percent = calculatePercent(seekBar, event);
-            scope.value = percent * scope.max;
-        };
+               var percent = calculatePercent(seekBar, event);
+               scope.value = percent * scope.max;
+           };
 
-        scope.trackThumb = function() {
-     $document.bind('mousemove.thumb', function(event) {
-         var percent = calculatePercent(seekBar, event);
-         scope.$apply(function() {
-             scope.value = percent * scope.max;
-         });
-     });
+           scope.trackThumb = function() {
+                $document.bind('mousemove.thumb', function(event) {
+                    var percent = calculatePercent(seekBar, event);
+                    scope.$apply(function() {
+                        scope.value = percent * scope.max;
+                    });
+                });
 
-     $document.bind('mouseup.thumb', function() {
-         $document.unbind('mousemove.thumb');
-         $document.unbind('mouseup.thumb');
-         });
-       };
-      }
-    };
-   }
+                $document.bind('mouseup.thumb', function() {
+                    $document.unbind('mousemove.thumb');
+                    $document.unbind('mouseup.thumb');
+                });
+            };
+
+         }
+       }
+     }
+
+
 
     angular
         .module('blocJams')
